d9360959ac51398ee1b6673378719966b1cbc401
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-03-20 08:45:34 +0100

    Cool stuff

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const app = express();
+const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 6000;
-
-//Static file declaration
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-//production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'build/index.html'));
-  })
-}
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'public/index.html'));
-})
-
-//start server
-app.listen(port, (req, res) => {
-  console.log( `server listening on port: ${port}`);
-})
+app.listen(port);
